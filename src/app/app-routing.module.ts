e53129bca46c18b228eb4f6536070d81d020f640
kenlin2709 <lin875223761@gmail.com>
2023-06-30 00:27:36 +1000

    footer + about me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
   {path: 'home', redirectTo:''},
   {path: '', component: HomeComponent},
+  {path: 'about', component: AboutMeComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component'; 
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutMeComponent } from './about-me/about-me.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
