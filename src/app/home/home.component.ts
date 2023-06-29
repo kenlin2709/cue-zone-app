20c7cb23e0a69b83928d90b3531fa4f318d9628c
kenlin2709 <lin875223761@gmail.com>
2023-06-29 23:09:14 +1000

    update slide show

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,21 +10,27 @@ import {interval} from 'rxjs';
 export class HomeComponent implements OnInit{
 
   slideNumber = 0;
+  nextSlideCalled = false;
 
   imgUrls = ['url(/assets/images/peri-landing-images/xianliang.jpg)', 'url(/assets/images/peri-landing-images/4.jpg)', 'url(/assets/images/peri-landing-images/202343135458.jpg)', 'url(/assets/images/peri-landing-images/paimingsai.jpg)']
 
  ngOnInit(): void {
   //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
   //Add 'implements OnInit' to the class.
-  interval(5000).subscribe(() => this.nextSlide());
+  interval(3000).subscribe(() => this.nextSlide());
  } 
 
   nextSlide() {
+    if(this.nextSlideCalled){
+      this.nextSlideCalled=false;
+      return;
+    }
     if (this.slideNumber === 3) {
       this.slideNumber = 0;
     } else {
       this.slideNumber++;
     }
+    this.nextSlideCalled=true;
   }
   getCurrentImgUrl(): string{
     return  this.imgUrls[this.slideNumber];
