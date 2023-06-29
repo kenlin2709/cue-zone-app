c3958f171032fe0c97dd7f47f6d25f8caf25530d
kenlin2709 <lin875223761@gmail.com>
2023-06-29 17:03:17 +1000

    cuezone autrlia

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {interval} from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit{
+
+  slideNumber = 0;
+
+  imgUrls = ['url(/assets/images/peri-landing-images/xianliang.jpg)', 'url(/assets/images/peri-landing-images/4.jpg)', 'url(/assets/images/peri-landing-images/202343135458.jpg)', 'url(/assets/images/peri-landing-images/paimingsai.jpg)']
+
+ ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  interval(5000).subscribe(() => this.nextSlide());
+ } 
+
+  nextSlide() {
+    if (this.slideNumber === 3) {
+      this.slideNumber = 0;
+    } else {
+      this.slideNumber++;
+    }
+  }
+  getCurrentImgUrl(): string{
+    return  this.imgUrls[this.slideNumber];
+  }
+  goToSlide(index:number){
+    this.slideNumber=index;
+  }
+
+}
