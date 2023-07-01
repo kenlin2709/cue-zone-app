12f82192d6d6cee139c5f357d62f834af6685fee
kenlin <lin875223761@gmail.com>
2023-07-01 16:45:12 +1000

    Added product carousel

--- src/app/ui-components/ui-product-carousel/ui-product-carousel.component.ts
+++ src/app/ui-components/ui-product-carousel/ui-product-carousel.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+
+import { SwiperComponent } from "swiper/angular";
+import SwiperCore, { Keyboard, Pagination, Navigation, Virtual, SwiperOptions } from 'swiper';
+SwiperCore.use([Keyboard, Pagination, Navigation, Virtual]);
+
 
 @Component({
-  selector: 'app-ui-product-carousel',
+  selector: 'product-carousel',
   templateUrl: './ui-product-carousel.component.html',
-  styleUrls: ['./ui-product-carousel.component.scss']
+  styleUrls: ['./ui-product-carousel.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class UiProductCarouselComponent {
+export class UiProductCarouselComponent{
+
+  config: SwiperOptions = {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    navigation: true,
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1.5,
+        spaceBetween: 10
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2.2,
+        spaceBetween: 10,
+      },
+      900: {
+        slidesPerView: 3.2,
+        spaceBetween: 10,
+      },
+      1271: {
+        slidesPerView: 4.2,
+        spaceBetween: 10,
+      }
+      
+    }
+  };
+
+  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+
+  onSwiper(swiper: any) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
 
 }
---
+++ src/app/ui-components/ui-product-carousel/ui-product-carousel.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { UiProductCarouselComponent } from "./ui-product-carousel.component";
+import { SwiperModule } from 'swiper/angular';
+import { UiProductCardComponent } from "../ui-product-card/ui-product-card.component";
+
+
+
+@NgModule({
+    declarations: [
+      UiProductCarouselComponent,
+      UiProductCardComponent
+    ],
+    imports: [
+        SwiperModule,
+    ],
+    providers: [],
+    exports: [UiProductCarouselComponent,
+              UiProductCardComponent,
+    ],
+  })
+  export class UiProductCarouselModule { }
+  
