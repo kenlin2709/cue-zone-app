85474e945740f3f5742f31714db6eff1dab5b37d
kenlin <lin875223761@gmail.com>
2023-07-02 12:14:06 +1000

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
 
 const routes: Routes = [
   {path: 'home', redirectTo:''},
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutMeComponent},
+  {path: 'product-detail/:id', component: ProductDetailPageComponent}
 
 ];
 
